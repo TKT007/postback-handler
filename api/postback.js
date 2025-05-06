e0daa9ff5cc42774584055de9fc691006cf02d9c
TKT007 <makosquad.ze@gmail.com>
2025-05-06 00:58:51 -0300

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const { fbclid } = req.query;
 
   // Enviar conversão ao Facebook
-  await fetch('https://graph.facebook.com/v18.0/YOUR_PIXEL_ID/events?access_token=YOUR_ACCESS_TOKEN', {
+  await fetch('https://graph.facebook.com/v18.0/1907845369956747/events?access_token=EAAfFtSgH5yMBO4YV2d6apiksEOn7kb9ZCqh6o23GVPb6myuDNYHcHJjlR8FCm2FH1HzBQbohUhcP2PmBNxAD7pomiBrGo4tM5o7lophbhyZCLQ6XVR03xZBNtyRnZCAv0Mo1TrGZAUjipZCllYI0eZCefgojCP1PE1HZCQfVZBumHjXq4Drg1VahVXtZCIN7sqVw5laQZDZD', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
         event_name: 'Lead',
         event_time: Math.floor(Date.now() / 1000),
         action_source: 'website',
-        event_source_url: 'https://seusite.com',
+        event_source_url: 'https://fastquote.pro',
         user_data: {
           fbc: fbclid,
         },
