9507ac6f8f7119188c3d587724a4d489f0bc995e
TKT007 <makosquad.ze@gmail.com>
2025-08-05 22:31:33 -0300

    Update postback.js

--- api/postback.js
+++ api/postback.js
@@ -1,22 +1,19 @@
-// api/postback.js
-
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).send('Method Not Allowed');
   }
 
-  const { fbclid, payout, test_event_code } = req.query;
+  const { fbclid, payout, currency, test_event_code } = req.query;
 
   if (!fbclid) {
     return res.status(400).json({ error: 'Parâmetro fbclid é obrigatório' });
   }
 
-  const ACCESS_TOKEN = 'EAAfFtSgH5yMBOxZCyzcrDVcZAwHcJ9EuTzOK1wzX2M4YmUDcmx1uW7ymMSSnT0OwwzZBbnMYDCFcM4hYIwiEPsKZCRI65ZByNqQjORqMi8zZARznHNFWMvLogqkh9c6vuNQPFrUZCZA435H3z8ZASSbOrZCMbtHsZCeeZBvPIURPX0e1mjWZAFw7DEt0O63rapgBLalVadgZDZD';
+  const ACCESS_TOKEN = 'EAAfFtSgH5yMBOxA4hqTUW8yi3yYZA6H7O5FbQZBArznqpaKPa7RFF0SZA8Eef482ZAKakc4RT96R210A5ZC87EJCARBUklgQKGATqKzJobSYvYBrtSWKXhtA27ZAZA8u1ifbz1CjZCbQZCN6GGC45K1LdysMamRVhVA1TBywCd1e2FK0xjFWFR05BAc10ig23Pbp9qwZDZD';
   const PIXEL_ID = '1907845369956747';
 
   const eventTime = Math.floor(Date.now() / 1000);
   const eventName = 'Lead';
-  const currency = 'USD';
 
   const payload = {
     data: [
@@ -24,17 +21,17 @@ export default async function handler(req, res) {
         event_name: eventName,
         event_time: eventTime,
         action_source: 'website',
-        event_source_url: 'https://fastquote.pro',
+        event_source_url: 'https://www.docsmaked.shop',
         user_data: {
           fbc: fbclid
         },
         custom_data: {
           value: parseFloat(payout || 0),
-          currency
+          currency: currency || 'USD'
         }
       }
     ],
-    ...(test_event_code && { test_event_code }) // adiciona test_event_code se estiver presente
+    ...(test_event_code && { test_event_code })
   };
 
   try {
